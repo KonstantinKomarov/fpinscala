a5ddc06dd58f7026c82a9f40b4bdece657e8e048
KonstantinKomarov <Konstantin.Komarov777@yandex.ru>
2026-09-15 22:16:15 +0300

    Monoid fold(Left|Right)

--- src/main/scala/fpinscala/exercises/monoids/Monoid.scala
+++ src/main/scala/fpinscala/exercises/monoids/Monoid.scala
@@ -67,10 +67,10 @@ object Monoid:
     as.foldLeft(m.empty)((acc, a) => m.combine(acc, f(a)))
 
   def foldRight[A, B](as: List[A])(acc: B)(f: (A, B) => B): B =
-    ???
+    foldMap(as, dual(endoMonoid))(f.curried)(acc)
 
   def foldLeft[A, B](as: List[A])(acc: B)(f: (B, A) => B): B =
-    ???
+    foldMap(as, endoMonoid)(a => b => f(b, a))(acc)
 
   def foldMapV[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B =
     ???
